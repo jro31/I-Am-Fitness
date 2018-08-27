030d5a828d105d1a836e21309adc37ada3fdca0b
Jethro Williams <jethrowilliams4@hotmail.com>
2018-08-27 23:18:39 +0700

    Initial Commit

---
+++ I Am Fitness/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  I Am Fitness
+//
+//  Created by Jethro Williams on 27/08/2018.
+//  Copyright © 2018 Jethro Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
